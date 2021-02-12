dc2b008ef8d090d0c5bbb5418672bf2ba3366b08
Kenan Dolic <kdolic@alumni.iastate.edu>
2021-02-12 08:29:44 -0600

    MVP - FLEX

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,47 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    // Arrange
+    render(<CheckoutForm />);
 
-test("form shows success message on submit with form details", () => {});
+    // Act 
+    const formHeader = screen.getByText(/checkout form/i);
+
+    // Assert
+    expect(formHeader).toBeInTheDocument();
+});
+
+test("form shows success message on submit with form details", async () => {
+      // Arrange
+      render(<CheckoutForm />);
+
+      // Act
+      // Set up query input elements
+      const firstNameInput = screen.getByLabelText(/first name/i)
+      const lastNameInput = screen.getByLabelText(/last name/i)
+      const addressInput = screen.getByLabelText(/address/i)
+      const cityInput = screen.getByLabelText(/city/i)
+      const stateInput = screen.getByLabelText(/state/i)
+      const zipInput = screen.getByLabelText(/zip/i)
+
+      // user types into those inputs
+      userEvent.type(firstNameInput, 'Kenan')
+      userEvent.type(lastNameInput, 'Dolic')
+      userEvent.type(addressInput, '7th St')
+      userEvent.type(cityInput, 'Los Angeles')
+      userEvent.type(stateInput, 'CA')
+      userEvent.type(zipInput, '90210')
+
+      // user clicks 'CHECKOUT'
+      const button = screen.getByRole('button')
+      userEvent.click(button)
+
+      // Assert
+      const newCheckout = await screen.findByText(/You have ordered some plants!/i)
+      expect(newCheckout).toBeInTheDocument();
+});
